f18c7407a28ce5fb773a63626fed716e89b7f6ad
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-25 01:33:08 +0600

    feat: Not Found Page Added

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function NewsNotFoundPage() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>404 - News Not Found</h1>
+      <p>Sorry, the news you are looking for does not exist.</p>
+    </div>
+  );
+}
