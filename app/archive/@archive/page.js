2e8219ead48ce11f1191659fed53615e4bcebdda
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-27 01:25:26 +0600

    feat: ArchivePage Component Added

--- app/archive/@archive/page.js
+++ app/archive/@archive/page.js
@@ -1,5 +1,20 @@
 import React from "react";
+import Link from "next/link";
+import { getAvailableNewsYears } from "@/lib/news";
 
 export default function ArchivePage() {
-  return <div>ArchivePage</div>;
+  const availableYears = getAvailableNewsYears();
+  return (
+    <header id="archive-header">
+      <nav>
+        <ul>
+          {availableYears.map((link) => (
+            <li key={link}>
+              <Link href={`/archive/${link}`}>{link}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
 }
