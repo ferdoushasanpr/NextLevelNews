3f7a5830d57ab3afe7e392e6c4c7cb0eebf6aa2a
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-29 00:21:06 +0600

    modify: Optional Catch All Route Added from Dynamic Year Route

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,9 +1,14 @@
 import React from "react";
 import Link from "next/link";
-import { getAvailableNewsYears } from "@/lib/news";
+import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import NewsLink from "@/components/news-link";
 
-export default function ArchivePage() {
+export default async function FilteredNewsPage({ params }) {
+  const { filter } = await params;
+  console.log(filter);
   const availableYears = getAvailableNewsYears();
+  //const news = getNewsForYear(filter);
+
   return (
     <header id="archive-header">
       <nav>
