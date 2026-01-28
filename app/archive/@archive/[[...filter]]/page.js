f6fc8aefa35dd8a78a3ae3b66ee15ba3ee949a37
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-29 00:37:12 +0600

    modify: FilteredNewsPage Modified with Optional Catch All Route

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -1,25 +1,53 @@
 import React from "react";
 import Link from "next/link";
-import { getAvailableNewsYears, getNewsForYear } from "@/lib/news";
+import {
+  getAvailableNewsYears,
+  getNewsForYear,
+  getAvailableNewsMonths,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import NewsLink from "@/components/news-link";
 
 export default async function FilteredNewsPage({ params }) {
   const { filter } = await params;
-  console.log(filter);
-  const availableYears = getAvailableNewsYears();
-  //const news = getNewsForYear(filter);
+  const selectedYear = filter?.[0];
+  const selectedMonth = filter?.[1];
+  let links = getAvailableNewsYears();
+
+  let news;
+  if (selectedYear && !selectedMonth) {
+    links = getAvailableNewsMonths(selectedYear);
+    news = getNewsForYear(filter);
+  }
+
+  if (selectedYear && selectedMonth) {
+    links = [];
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {availableYears.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {news && news.length > 0 ? (
+        <NewsLink news={news} />
+      ) : (
+        <p>No news found.</p>
+      )}
+    </>
   );
 }
