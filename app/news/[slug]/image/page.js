a7e04440cee98efeda0a4cd34e2032aec5d993ed
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-29 22:10:31 +0600

    feat: ImagePage Component Added

--- app/news/[slug]/image/page.js
+++ app/news/[slug]/image/page.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import { DUMMY_NEWS } from "@/dummy-news";
+
 export default async function ImagePage({ params }) {
   const { slug } = await params;
+  const newsItem = DUMMY_NEWS.find((item) => item.slug === slug);
 
-  return <div>ImagePage</div>;
+  if (!newsItem) {
+    notFound();
+  }
+
+  return (
+    <div className="fullscreen-image">
+      <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+    </div>
+  );
 }
