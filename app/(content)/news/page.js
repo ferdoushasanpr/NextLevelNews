c9485348fd377474dc2211ca1f5e4205440642dc
ferdoushasanpr <ferdoushasanpr@gmail.com>
2026-01-31 21:09:09 +0600

    modify: News Fetching in NewsPage Modified

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,19 @@
 import { DUMMY_NEWS } from "@/dummy-news";
 import NewsLink from "@/components/news-link";
 
-export default function NewsPage() {
+export default async function NewsPage() {
+  const response = await fetch("http://localhost:8080/news");
+
+  if (!response.ok) {
+    throw new Error("Fetching News Failed...");
+  }
+
+  const news = await response.json();
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsLink news={DUMMY_NEWS} />
+      <NewsLink news={news} />
     </>
   );
 }
